c6ceaf6bfebf06261fcee9d722141cdf5de21fa8
diegoliv <diego@favolla.com.br>
2014-07-15 15:17:16 -0300

    added languages files

--- fl-custom-modules.php
+++ fl-custom-modules.php
@@ -17,15 +17,26 @@ class FLCustomModules {
 
 	public $plugin_slug = 'fl-custom-modules';
 
+	function __construct(){
+		add_action( 'init', array( $this, 'custom_load_modules' ), 12 );
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ), 15 );
+	}
+
+	function custom_load_modules(){
+		include_once 'modules/button/button.php';
+		include_once 'modules/heading/heading.php';
+	}
+
+	function load_plugin_textdomain() {
+
+		$domain = $this->plugin_slug;
+		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+
+		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
+		load_plugin_textdomain( $domain, FALSE, basename( plugin_dir_path( dirname( __FILE__ ) ) ) . '/languages/' );
+
+	}
+
 }
 
 $modules = new FLCustomModules();
-
-add_action( 'init', 'custom_load_modules', 12 );
-
-
-function custom_load_modules(){
-	include_once 'modules/button/button.php';
-	include_once 'modules/heading/heading.php';
-}
-
